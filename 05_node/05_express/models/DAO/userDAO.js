9d6267585e78eb1c44d2153dda6c03a6e6e202d1
sonbyungjun <saint2030@naver.com>
2018-07-13 13:41:53 +0900

    7월13일 수업내용

--- 05_node/05_express/models/DAO/userDAO.js
+++ 05_node/05_express/models/DAO/userDAO.js
@@ -22,6 +22,40 @@ var userDAO = {
             }
             callback(null, results);
         })
+    },
+
+    list : function(callback){
+        var sql = `select * from users;`
+        con.query(sql, function(err, results, fields){
+            if(err){
+                callback(err)
+                return;
+            }
+            callback(null, results);
+        })
+    },
+
+    detail : function(no, callback){
+        var sql = `select * from users where no = ?`
+        con.query(sql, no, function(err, results, fields){
+            if(err){
+                callback(err)
+                return;
+            }
+            callback(null, results);
+        })
+    },
+
+    
+    delete : function(no, callback){
+        var sql = `delete from users where no = ?`
+        con.query(sql, no, function(err, results, fields){
+            if(err){
+                callback(err)
+                return;
+            }
+            callback(null, results);
+        })
     }
 
 }
--- 05_node/05_express/routes/user.js
+++ 05_node/05_express/routes/user.js
@@ -5,7 +5,7 @@ var userDAO = require('../models/dao/userDAO');
 
 router.get("/login", function(req, res){
     console.log("로그인");
-    res.render('login')
+    res.render('user/login')
 });
 
 router.post("/login", function(req, res){
@@ -18,29 +18,66 @@ router.post("/login", function(req, res){
             // next(err);
             return;
         }
-        res.render("index", {user : result[0]});
+        // res.render("index", {user : result[0]});
+        res.redirect('/');
     })
 });
 
 router.get("/signup", function(req, res){
     console.log("회원가입");
-    res.render('signup')
+    res.render('user/signup')
 });
 
 router.post("/signup", function(req, res){
-    var id = req.body.id
-    var pw = req.body.pw
-    var name = req.body.name
-    var age = req.body.age
-    var gender = req.body.gender
-    var arr = [id,pw,name,age,gender]
+    var arr = []
+    for(var key in req.body){
+        arr.push(req.body[key])
+    }
 
     userDAO.signup(arr, function(err, result){
         if(err){
             return;
         }
-        res.render("signup", result);
+        // res.render("login");
+        res.redirect('user/login');
     })
 });
+
+router.get("/list", function(req, res, next){
+    userDAO.list(function(err, result){
+        if(err){
+            return next(err);
+        }
+        for(var r of result){
+            r.reg_date = moment(r.reg_date)
+        }
+        res.render('user/userList', {users : result});
+    })
+})
+
+router.get("/detail", function(req, res, next){
+    userDAO.detail(req.query.no, function(err, result){
+        if(err){
+            return next(err);
+        }
+        result[0].reg_date = moment(result[0].reg_date)
+        res.render('user/userdetail',{user : result[0]});
+    })
+})
+
+router.get("/delete", function(req, res, next){
+    userDAO.delete(req.query.no, function(err, result){
+        if(err){
+            return next(err);
+        }
+        res.redirect('/user/list');
+    })
+})
+
+function moment(time){
+    var d = new Date(time)
+    d = `${d.getFullYear()}년 ${d.getMonth()}월 ${d.getDate()}일 ${d.getHours()}시 ${d.getMinutes()}분`
+    return d;
+}
 
 module.exports = router;
